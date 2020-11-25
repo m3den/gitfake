383e9a2a7026c9a741f83099018fa325e6c27347
Victor Tarasevich <m3den0@gmail.com>
2020-11-25 22:23:21 +0300

    add repo path

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from datetime import datetime, date
 from datetime import timedelta
 from random import randint
 
+REPO_PATH = '/repo/'
+
 
 count = 1
 today = datetime.now()
@@ -12,8 +14,8 @@ gitFake = randint(1, 10)
 while gitFake > 0:
     today = today - timedelta(0, count * randint(1, 3), count * randint(2, 9), count * randint(3, 6))
     strtoday = today.strftime('%a %b %d %H:%M:%S %Y -0600')
-    command = 'git commit --allow-empty -m "' + strtoday + '" --date="' + strtoday  + '"'
+    command = f'cd {REPO_PATH} && git commit --allow-empty -m "{strtoday}" --date="{strtoday}"'
     os.system(command)
     gitFake -= 1
 
-os.system('git push origin main')
+os.system(f'cd {REPO_PATH} && git push origin main')
